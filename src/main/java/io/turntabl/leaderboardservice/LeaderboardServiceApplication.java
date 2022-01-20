63ec1660dcf60a33fe10aec6f431278baca9607e
Henry Osei Agyemang <91277878+Henosagy@users.noreply.github.com>
2022-01-20 08:21:01 +0000

    Fixed issue with getLanguageLevel by resolvingthe surrogate key of Profile from type profile tostring in LanguageLevelID class

--- src/main/java/io/turntabl/leaderboardservice/LeaderboardServiceApplication.java
+++ src/main/java/io/turntabl/leaderboardservice/LeaderboardServiceApplication.java
@@ -1,13 +1,15 @@
 package io.turntabl.leaderboardservice;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@OpenAPIDefinition
+@OpenAPIDefinition(info = @Info(title = "Turntable Codewars Leaderboard API", version = "2.0",
+		description = "Agile mini-project for generation codewars leaderboard based on certain paramerters."))
 public class LeaderboardServiceApplication {
 
 	public static void main(String[] args) {
